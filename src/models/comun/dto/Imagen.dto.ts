3d3fc354b77b96224e85d537ebf4e289efb7b767
Diego Martinez <martinezd@presidencia.gob.ec>
2023-12-11 00:14:12 -0500

    MS-SEGURIDAD / Refactorización código.

---
+++ src/models/comun/dto/Imagen.dto.ts
@@ -0,0 +1,12 @@
+export class ImagenDto {
+  destination: string; // * "/home/usuario/Proyectos/nodejs/servidor/server_cf/api-enlace-cooperativa/public/13561656232"
+  encoding: string; // * "7bit"
+  fieldname: string; // *  "13561656232"
+  filename: string; // * "13561656232.jpeg"
+  mimetype: string; // * "image/jpeg"
+  originalname: string; // * "WhatsApp Image 2023-11-17 at 14.02.51.jpeg"
+  path: string; // * "/home/usuario/Proyectos/nodejs/servidor/server_cf/api-enlace-cooperativa/public/13561656232/13561656232.jpeg"
+  size: number; // * 131101
+  url: string;
+  base64: string;
+};
--- src/models/usuarios/dto/actualiza-usuarioimagen.dto.ts
+++ src/models/usuarios/dto/actualiza-usuarioimagen.dto.ts
@@ -1,17 +1,8 @@
+import { ImagenDto } from "@models/comun/dto/Imagen.dto";
+
 export class ActualizaUsuarioImagenDto {
   readonly _id: string; // * "sdg24fsd5gDSasdf"
   readonly identificacion: string; // * "sdg24fsd5gDSasdf"
-  readonly imagen: {
-    destination: string; // * "/home/usuario/Proyectos/nodejs/servidor/server_cf/api-enlace-cooperativa/public/13561656232"
-    encoding: string; // * "7bit"
-    fieldname: string; // *  "13561656232"
-    filename: string; // * "13561656232.jpeg"
-    mimetype: string; // * "image/jpeg"
-    originalname: string; // * "WhatsApp Image 2023-11-17 at 14.02.51.jpeg"
-    path: string; // * "/home/usuario/Proyectos/nodejs/servidor/server_cf/api-enlace-cooperativa/public/13561656232/13561656232.jpeg"
-    size: number; // * 131101
-    url: string;
-    base64: string;
-  };
+  readonly imagen: ImagenDto;
   readonly usuario?: string;
 }
